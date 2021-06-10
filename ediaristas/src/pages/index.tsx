e40b697b8312420cbc16fb61454fd46784ffaed1
Rossicler Júnior <rossiclerjr@gmail.com>
2021-06-10 00:59:14 -0300

    Added components to the index page

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,10 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
   return (
     <div>
-      
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+
+      <UserInformation
+        name={"Rossicler Júnior"}
+        picture={"https://github.com/rossicler.png"}
+        rating={3}
+        description={"Brasília"}
+      />
     </div>
-  )
+  );
 }
