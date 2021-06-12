e584d2be44f321605b96a1232f0c4a88b9b4566e
Rossicler Júnior <rossiclerjr@gmail.com>
2021-06-11 21:40:33 -0300

    Created TextField and TextFieldMask component and changed the userinformation list layout

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { Button, Typography, Container } from "@material-ui/core";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import {
+  FormElementsContainer,
+  ProfessionalsPaper,
+  ProfessionalsContainer,
+} from "@styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -13,12 +20,65 @@ export default function Home() {
         }
       />
 
-      <UserInformation
-        name={"Rossicler Júnior"}
-        picture={"https://github.com/rossicler.png"}
-        rating={3}
-        description={"Brasília"}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-9999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}>CEP inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfessionalsPaper>
+          <ProfessionalsContainer>
+            <UserInformation
+              name={"Rossicler Júnior"}
+              picture={"https://github.com/rossicler.png"}
+              rating={3}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Rossicler Júnior"}
+              picture={"https://github.com/rossicler.png"}
+              rating={3}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Rossicler Júnior"}
+              picture={"https://github.com/rossicler.png"}
+              rating={3}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Rossicler Júnior"}
+              picture={"https://github.com/rossicler.png"}
+              rating={3}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Rossicler Júnior"}
+              picture={"https://github.com/rossicler.png"}
+              rating={3}
+              description={"Brasília"}
+            />
+            <UserInformation
+              name={"Rossicler Júnior"}
+              picture={"https://github.com/rossicler.png"}
+              rating={3}
+              description={"Brasília"}
+            />
+          </ProfessionalsContainer>
+        </ProfessionalsPaper>
+      </Container>
     </div>
   );
 }
